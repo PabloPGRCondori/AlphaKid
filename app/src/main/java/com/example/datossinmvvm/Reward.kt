c2e7f1489ee7b4e8351a18f89ca7ce41a033bd87
Esteban Mathías Pacheco Carrión <167585673+Estebanpacheco18@users.noreply.github.com>
2024-10-15 19:36:04 -0500

    Add a new view about the system of reward

---
+++ app/src/main/java/com/example/datossinmvvm/Reward.kt
@@ -0,0 +1,8 @@
+package com.example.datossinmvvm
+
+data class Reward(
+    val id: String,
+    val title: String,
+    val description: String,
+    val points: Int
+)
--- app/src/main/java/com/example/datossinmvvm/UserDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.datossinmvvm
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [User::class], version = 1)
-abstract class UserDatabase: RoomDatabase() {
-    abstract fun userDao(): UserDao
-}
