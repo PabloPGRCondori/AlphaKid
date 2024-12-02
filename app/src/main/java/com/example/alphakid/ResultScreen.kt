8b3df49399ee0f267ed8f2041916019ed69bd275
PabloPGRCondori <tsukuraishimotlo@gmail.com>
2024-12-02 08:05:34 -0500

    UPDATE-Actualizacion del diseño de la vista Result

--- app/src/main/java/com/example/alphakid/ResultScreen.kt
+++ app/src/main/java/com/example/alphakid/ResultScreen.kt
@@ -1,6 +1,11 @@
 package com.example.alphakid
 
 import android.content.Context
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -9,6 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun ResultScreen(
@@ -23,13 +34,16 @@ fun ResultScreen(
     var showContinueButton by remember { mutableStateOf(false) }
     var mutableWordCount by remember { mutableStateOf(wordCount) }
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    var showResult by remember { mutableStateOf(false) }
 
     LaunchedEffect(detectedText) {
         if (randomPalabra != null) {
             if (detectedText.equals(randomPalabra.nombre, ignoreCase = true)) {
                 mutableWordCount++
                 val wordPoints = randomPalabra.puntos
-                val sharedPreferences = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+                val sharedPreferences =
+                    context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
                 val currentPoints = sharedPreferences.getInt("user_points", 0)
                 with(sharedPreferences.edit()) {
                     putInt("user_points", currentPoints + wordPoints)
@@ -38,46 +52,123 @@ fun ResultScreen(
                 if (mutableWordCount >= 10) {
                     resultMessage = "¡Lo lograste! Has escaneado 10 palabras correctamente."
                     showContinueButton = false
+                    showResult = true
                     navController.navigate("main") // Navigate back to the main screen
                 } else {
-                    resultMessage = "¡Felicidades! Formaste la palabra ${randomPalabra.nombre} correctamente y ganaste $wordPoints puntos."
+                    resultMessage =
+                        "¡Felicidades! Formaste la palabra ${randomPalabra.nombre} correctamente y ganaste $wordPoints puntos."
                     showContinueButton = true
+                    showResult = true
                 }
             } else {
-                resultMessage = "Texto detectado: $detectedText. No coincide con la palabra del reto."
+                resultMessage =
+                    "Texto detectado: $detectedText. No coincide con la palabra del reto."
             }
         } else {
             resultMessage = "No se encontró una palabra del reto."
         }
     }
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        if (randomPalabra != null) {
-            Text(
-                text = "Palabra del reto: ${randomPalabra.nombre}",
-                style = MaterialTheme.typography.headlineMedium
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        Color.LightGray,
+                        Color.White
+                    )
+                )
             )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        Text(
-            text = resultMessage,
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        if (showContinueButton) {
-            Button(onClick = { onContinue() }) {
-                Text(text = "Continuar")
+    ) {
+        // Primera Card para la "Palabra del reto"
+        if (randomPalabra != null) {
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Palabra del reto: ${randomPalabra.nombre}",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                }
             }
+            Spacer(modifier = Modifier.height(16.dp)) // Espacio entre las Cards
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = { navController.navigate("main") }) {
-            Text(text = "Salir")
+
+        // Segunda Card para el mensaje de resultado y los botones
+        AnimatedVisibility(
+            visible = showResult,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = resultMessage,
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                }
+            }
+
+        }
+        Row(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(bottom = 16.dp), // Espacio inferior para la imagen
+            horizontalArrangement = Arrangement.spacedBy(16.dp) // Espacio entre botones
+        ) {
+            if (showContinueButton) {
+                Button(
+                    onClick = { onContinue() },
+                    modifier = Modifier
+                        .weight(1f) // Ocupar la mitad del espacio disponible
+                        .padding(8.dp)
+                ) {
+                    Text(text = "Continuar")
+                }
+            }
+            Button(
+                onClick = { navController.navigate("main") },
+                modifier = Modifier
+                    .weight(1f) // Ocupar la mitad del espacio disponible
+                    .padding(8.dp)
+            ) {
+                Text(text = "Salir")
+            }
+
+        }
+        Image(
+            painter = painterResource(id = R.drawable.tu_imagen),
+            contentDescription = "Imagen",
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp) //
+        )
+        LaunchedEffect(resultMessage) {
+            if (resultMessage.isNotEmpty()) {
+                showResult = true
+            }
         }
     }
 }
